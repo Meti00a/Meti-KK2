7b1e5c62539323fec43e7be3adeb84737c2a2d83
Meti00a <muhamedhajra00@gmail.com>
2026-06-08 21:11:49 +0200

    added endpoints for data stats in the project.

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def health():
 
 @app.post("/data/upload")
 async def upload_data(file: UploadFile = File(...)):
+    global current_dataset
     df = pd.read_csv(file.file)
-    
+    current_dataset =df
     return {
         "rows": len(df),
         "columns": list(df.columns),
         "dtypes": {column: str(dtype) for column, dtype in df.dtypes.items()}
     }
+    
+    
+@app.get("/data/stats")
+def get_stats():
+    if current_dataset is None:
+        return {"error": "No dataset uploaded"}
+    return current_dataset.describe().to_dict()
